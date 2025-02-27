e36653c7baa51e4e40d34676e852a308e858c7d0
DMaximoVarela <maxivarela95@gmail.com>
2025-02-26 22:27:22 -0300

    init: initial config

---
+++ rollup.config.mjs
@@ -0,0 +1,42 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+import packageJson from "./package.json" assert { type: "json" };
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    external: [
+      ...Object.keys(packageJson.peerDependencies || {}),
+      ...Object.keys(packageJson.dependencies || {}),
+    ],
+    plugins: [
+      typescript({
+        tsconfig: "./tsconfig.json",
+      }),
+      resolve(),
+      commonjs(),
+      postcss(),
+    ],
+  },
+  {
+    input: "dist/esm/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.(css|less|scss)$/, /\.stories\.(tsx|ts)$/],
+  },
+];
