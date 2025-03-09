521bb09dea62d2341e11c04090e4a9b76b60b84d
DMaximoVarela <maxivarela95@gmail.com>
2025-03-09 13:02:30 -0300

    feat: first table design (wip)

--- src/components/DataGrid/DataGrid.tsx
+++ src/components/DataGrid/DataGrid.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { DataGridProps } from "./types";
+import "./styles.css";
 
 const DataGrid: React.FC<DataGridProps> = ({
   rows,
@@ -13,7 +14,36 @@ const DataGrid: React.FC<DataGridProps> = ({
   borderStyle,
   borderWidth,
 }) => {
-  return <div>DataGrid</div>;
+  return (
+    <React.Fragment>
+      <table id="zayrok-table">
+        <thead id="zayrok-table-header">
+          <tr>
+            <th className="zayrok-column" id="first-column">
+              <span>Columna</span>
+            </th>
+            <th className="zayrok-column" id="last-column">
+              <span>Segunda Columna</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody id="zayrok-table-body">
+          <tr>
+            <td className="zayrok-cell">Fila 1</td>
+            <td className="zayrok-cell">Fila 2</td>
+          </tr>
+          <tr>
+            <td className="zayrok-cell" id="last-row-left">
+              Fila 3
+            </td>
+            <td className="zayrok-cell" id="last-row-right">
+              Fila 4
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
 };
 
 export default DataGrid;
