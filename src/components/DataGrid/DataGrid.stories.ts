bfcb90ad44b0cefa7aa213bd35668b2d84f2b093
DMaximoVarela <maxivarela95@gmail.com>
2025-03-15 00:04:50 -0300

    feat: first story

--- src/components/DataGrid/DataGrid.stories.ts
+++ src/components/DataGrid/DataGrid.stories.ts
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 // import { fn } from "@storybook/test";
 
 import DataGrid from "./DataGrid";
+import { Rows } from "../../types";
 
 const meta = {
   title: "Components/DataGrid",
@@ -19,4 +20,158 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {};
+const primaryColumns = [
+  { field: "name", header: "Name", sortable: false },
+  { field: "email", header: "Email", sortable: false },
+  { field: "role", header: "Role", sortable: false },
+];
+
+const primaryRows: Rows = [
+  {
+    rowId: 1,
+    cells: [
+      {
+        field: "User 1",
+        type: "text",
+        label: "user",
+      },
+      {
+        field: "user1@example.com",
+        type: "text",
+        label: "user",
+      },
+      {
+        field: "Developer",
+        type: "text",
+        label: "user",
+      },
+    ],
+  },
+  {
+    rowId: 2,
+    cells: [
+      {
+        field: "User 2",
+        type: "text",
+        label: "user",
+      },
+      {
+        field: "user2@example.com",
+        type: "text",
+        label: "user",
+      },
+      {
+        field: "Developer",
+        type: "text",
+        label: "user",
+      },
+    ],
+  },
+  {
+    rowId: 3,
+    cells: [
+      {
+        field: "User 3",
+        type: "text",
+        label: "user",
+      },
+      {
+        field: "user3@example.com",
+        type: "text",
+        label: "user",
+      },
+      {
+        field: "Designer",
+        type: "text",
+        label: "user",
+      },
+    ],
+  },
+  {
+    rowId: 4,
+    cells: [
+      {
+        field: "User 4",
+        type: "text",
+        label: "user",
+      },
+      {
+        field: "user4@example.com",
+        type: "text",
+        label: "user",
+      },
+      {
+        field: "Developer",
+        type: "text",
+        label: "user",
+      },
+    ],
+  },
+  {
+    rowId: 5,
+    cells: [
+      {
+        field: "User 5",
+        type: "text",
+        label: "user",
+      },
+      {
+        field: "user5@example.com",
+        type: "text",
+        label: "user",
+      },
+      {
+        field: "Designer",
+        type: "text",
+        label: "user",
+      },
+    ],
+  },
+  {
+    rowId: 6,
+    cells: [
+      {
+        field: "User 6",
+        type: "text",
+        label: "user",
+      },
+      {
+        field: "user6@example.com",
+        type: "text",
+        label: "user",
+      },
+      {
+        field: "Developer",
+        type: "text",
+        label: "user",
+      },
+    ],
+  },
+  {
+    rowId: 7,
+    cells: [
+      {
+        field: "User 7",
+        type: "text",
+        label: "user",
+      },
+      {
+        field: "user7@example.com",
+        type: "text",
+        label: "user",
+      },
+      {
+        field: "Developer",
+        type: "text",
+        label: "user",
+      },
+    ],
+  },
+];
+
+export const Primary: Story = {
+  args: {
+    columns: primaryColumns,
+    rows: primaryRows,
+  },
+};
